18193aa813df4348da3c79e478ce36dd828ef666
dogan.guendogdu <dogan.guendogdu@7p-group.com>
2025-02-05 15:40:46 +0100

    [Refactor] backend structure.

---
+++ backend/app/controller/payment_controller.py
@@ -0,0 +1,33 @@
+from ..entities.payments import *
+from ..models.payment_model import ApplePayStrategy
+from ..models.payment_model import MasterCardStrategy
+from ..models.payment_model import PayPalStrategy
+from ..models.payment_model import PaySafeStrategy
+from ..models.payment_model import PaymentModel
+
+
+class PaymentController:
+    def __init__(self):
+        self.__payment_model = PaymentModel()
+
+    def get_PayPal_payment_details(self, paypal_user_name: str, paypal_password: SecretStr):
+        paypal_model = PayPal(email=paypal_user_name, password=paypal_password)
+        self.__payment_model.set_payment_method(PayPalStrategy(paypal_model))
+        return self.__payment_model.get_payment_details()
+
+    def get_master_card_payment_details(self, credit_card_number: int, expiration_date: datetime, cvc_number: int,
+                                        first_name: str, last_name: str):
+        credit_card_model = MasterCard(credit_card_number=credit_card_number, expiration_date=expiration_date,
+                                       cvc=cvc_number, first_name=first_name, last_name=last_name)
+        self.__payment_model.set_payment_method(MasterCardStrategy(credit_card_model))
+        return self.__payment_model.get_payment_details()
+
+    def get_PaySafe_payment_details(self, paysafe_code: int):
+        paysafe_model = PaySafe(paysafe_code=paysafe_code)
+        self.__payment_model.set_payment_method(PaySafeStrategy(paysafe_model))
+        return self.__payment_model.get_payment_details()
+
+    def get_ApplePay_payment_details(self, apple_id: EmailStr, apple_id_password: SecretStr):
+        apple_pay_model = ApplePay(apple_id=apple_id, password=apple_id_password)
+        self.__payment_model.set_payment_method(ApplePayStrategy(apple_pay_model))
+        return self.__payment_model.get_payment_details()
--- backend/app/routes/payment_routes.py
+++ backend/app/routes/payment_routes.py
@@ -1,36 +1,36 @@
 from fastapi import APIRouter
 
-from ..controller.process_payment_controller import ProcessPaymentController
-from ..models.payment_methods_model import ApplePayModel
-from ..models.payment_methods_model import MasterCardModel
-from ..models.payment_methods_model import PayPalModel
-from ..models.payment_methods_model import PaySafeModel
+from ..controller.payment_controller import PaymentController
+from ..entities.payments import ApplePay
+from ..entities.payments import MasterCard
+from ..entities.payments import PayPal
+from ..entities.payments import PaySafe
 
 router = APIRouter()
 
-process_payment_controller = ProcessPaymentController()
+payment_controller = PaymentController()
 
 
 @router.post("/payment/paypal")
-def get_PayPal_payment_details(paypal_payment: PayPalModel):
-    return process_payment_controller.get_PayPal_payment_details(
+def get_PayPal_payment_details(paypal_payment: PayPal):
+    return payment_controller.get_PayPal_payment_details(
         paypal_payment.email,
         paypal_payment.password)
 
 
 @router.post("/payment/master_card")
-def get_credict_card_payment_details(credit_card_payment: MasterCardModel):
-    return process_payment_controller.get_master_card_payment_details(
+def get_credict_card_payment_details(credit_card_payment: MasterCard):
+    return payment_controller.get_master_card_payment_details(
         credit_card_payment.credit_card_number, credit_card_payment.expiration_date, credit_card_payment.cvc,
         credit_card_payment.first_name, credit_card_payment.last_name)
 
 
 @router.post("/payment/paysafe")
-def get_PaySafe_payment_details(paysafe_payment: PaySafeModel):
-    return process_payment_controller.get_PaySafe_payment_details(paysafe_payment.paysafe_code)
+def get_PaySafe_payment_details(paysafe_payment: PaySafe):
+    return payment_controller.get_PaySafe_payment_details(paysafe_payment.paysafe_code)
 
 
 @router.post("/payment/apple_pay")
-def get_ApplePay_payment_details(apple_pay_payment: ApplePayModel):
-    return process_payment_controller.get_ApplePay_payment_details(
+def get_ApplePay_payment_details(apple_pay_payment: ApplePay):
+    return payment_controller.get_ApplePay_payment_details(
         apple_pay_payment.apple_id, apple_pay_payment.password)
