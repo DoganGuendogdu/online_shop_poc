fa3c668e861ce52282cbc43b02d0c3dea2cbc951
dogan.guendogdu <dogan.guendogdu@7p-group.com>
2025-02-10 11:55:42 +0100

    [Add] read HTTP endpoints from config file.

--- frontend/app/config.py
+++ frontend/app/config.py
@@ -8,7 +8,12 @@ class Config:
     def __init__(self):
         self.__config_path = Path(PROJECT_ROOT) / "config.json"
 
-    def get_payment_methods(self):
         with open(self.__config_path, "r") as json_file:
             config = json.load(json_file)
-            return config["payment_methods"]
+
+            self.payment_methods = config["payment_methods"]
+
+            self.paypal_url = config["url"]["paypal"]
+            self.paysafe_url = config["url"]["paysafe"]
+            self.apple_pay_url = config["url"]["apple_pay"]
+            self.master_card_url = config["url"]["master_card"]
