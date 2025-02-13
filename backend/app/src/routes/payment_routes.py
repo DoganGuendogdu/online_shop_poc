d19861c8f567f1189060de5f4627a21b2e9b3a9e
dogan.guendogdu <dogan.guendogdu@7p-group.com>
2025-02-13 16:11:08 +0100

    [Add] HTTP 400 status handling example (frontend).

--- backend/app/src/routes/payment_routes.py
+++ backend/app/src/routes/payment_routes.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from fastapi import status
 
 from ..controller.payment_controller import PaymentController
 from ..entities.payments import ApplePay
@@ -13,9 +14,7 @@ payment_controller = PaymentController()
 
 @router.post("/payment/paypal")
 def get_PayPal_payment_details(paypal_payment: PayPal):
-    return payment_controller.get_PayPal_payment_details(
-        paypal_payment.email,
-        paypal_payment.password)
+    return payment_controller.get_PayPal_payment_details(paypal_payment.email, paypal_payment.password)
 
 
 @router.post("/payment/master_card")
@@ -23,6 +22,15 @@ def get_credict_card_payment_details(credit_card_payment: MasterCard):
     return payment_controller.get_master_card_payment_details(
         credit_card_payment.credit_card_number, credit_card_payment.expiration_date, credit_card_payment.cvc,
         credit_card_payment.first_name, credit_card_payment.last_name)
+
+
+# @router.post("/payment/paysafe")
+# def get_PaySafe_payment_details(paysafe_data: dict[str, int]):
+#     if len(str(paysafe_data.get("paysafe_code"))) > 16:
+#         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+#                             detail="The provided paysafe code contains more than 16 digits.")
+#
+#     return payment_controller.get_PaySafe_payment_details(**paysafe_data)
 
 
 @router.post("/payment/paysafe")
