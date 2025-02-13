787be75a3f26719683d90b5294e0266f91da326f
dogan.guendogdu <dogan.guendogdu@7p-group.com>
2025-02-13 12:46:12 +0100

    [Add] additional integration tests for API.

--- backend/app/tests/test_api.py
+++ backend/app/tests/test_api.py
@@ -17,25 +17,38 @@ def test_post_paypal_payment_200():
     assert response.json() == {"payment_method": "paypal", "email": "test@gmail.com"}
 
 
-def test_post_paypal_payment_402():
-    request_body = {"email": "gmail.com", "password": "823fnr828932hf"}
+def test_post_paypal_payment_invalid_email_422():
+    request_body = {"email": "test.com", "password": "823fnr828932hf"}
+    response = test_client.post("/payment/paypal", json=request_body)
+    assert response.status_code == 422
+
+
+def test_post_paypal_payment_invalid_password_422():
+    request_body = {"email": "olaf@test.com", "password": "123"}
     response = test_client.post("/payment/paypal", json=request_body)
     assert response.status_code == 422
 
 
 def test_post_master_card_payment_200():
-    request_body = {"credit_card_number": 1233123312341235, "expiration_date": "2025-02-05 09:23:44.790790", "cvc": 123,
+    request_body = {"credit_card_number": 1233123312341235, "expiration_date": "2044-02-05", "cvc": 123,
                     "first_name": "Kaan", "last_name": "KEK"}
     response = test_client.post("payment/master_card", json=request_body)
     assert response.status_code == 200
     assert response.json() == {"payment_method": "credit_card", "credit_card_number": 1233123312341235,
-                               "expiration_date": "2025-02-05T09:23:44.790790", "first_name": "Kaan",
+                               "expiration_date": "2044-02-05", "first_name": "Kaan",
                                "last_name": "KEK"}
 
 
 def test_post_master_card_payment_invalid_cvc_422():
-    request_body = {"credit_card_number": 1233123312341235, "expiration_date": "2025-02-05 09:23:44.790790",
+    request_body = {"credit_card_number": 1233123312341235, "expiration_date": "2066-02-05",
                     "cvc": 123892734, "first_name": "Kaan", "last_name": "KEK"}
+    response = test_client.post("payment/master_card", json=request_body)
+    assert response.status_code == 422
+
+
+def test_post_master_card_payment_invalid_credit_card_number_422():
+    request_body = {"credit_card_number": 3333, "expiration_date": "2066-02-05",
+                    "cvc": 123, "first_name": "Kaan", "last_name": "KEK"}
     response = test_client.post("payment/master_card", json=request_body)
     assert response.status_code == 422
 
